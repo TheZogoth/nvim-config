13aa0104af377a18a694130aead30af7a7f98e00
Johan Ahlqvist <johah956@student.liu.se>
2025-08-31 13:19:55 +0200

    Add ODIN support
    * Move keybindings from lspconfig to keybindings
    * Add command to setup ODIN language server with LSP config

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -24,6 +24,66 @@ vim.keymap.set(
   'n',
   'fg',
   function()
-    require('telescope.builtin').grep_string()
+    require('telescope.builtin').live_grep()
   end,
   {noremap = true, silent = true, desc = "Telescope find files by contents" })
+
+-- Code completion and things
+vim.keymap.set(
+  'n',
+  '<A-r>',
+  vim.lsp.buf.rename,
+  { noremap = true, silent = true, desc = "Rename symbol" })
+vim.keymap.set(
+  'n',
+  'H',
+  vim.lsp.buf.hover,
+  { noremap = true, silent = true, desc = "Symbol information" })
+
+vim.keymap.set(
+  'n',
+  'gdec',
+  vim.lsp.buf.declaration,
+  { noremap = true, silent = true, desc = "Go to declaration" })
+vim.keymap.set(
+  'n',
+  'gdef',
+  vim.lsp.buf.definition,
+  { noremap = true, silent = true, desc = "Go to definition" })
+vim.keymap.set(
+  'n',
+  'gimpl',
+  vim.lsp.buf.implementation,
+  { noremap = true, silent = true, desc = "Go to implementation" })
+
+-- Diagnostics
+vim.keymap.set(
+  'n',
+  '<C-a>e',
+  vim.diagnostic.open_float,
+  { noremap = true, silent = true, desc = "Show detailed diagnostics" })
+vim.keymap.set(
+  'n',
+  'ge',
+  function()
+    vim.diagnostic.goto_next({
+      severity = vim.diagnostic.severity.ERROR,
+      float = false})
+  end,
+  { noremap = true, silent = true, desc = "Go to next error" })
+vim.keymap.set(
+  'n',
+  'gw',
+  function()
+    vim.diagnostic.goto_next({
+      severity = vim.diagnostic.severity.WARN,
+      float = false})
+  end,
+  { noremap = true, silent = true, desc = "Go to next warning" })
+vim.keymap.set(
+  'n',
+  'gn',
+  function()
+    vim.diagnostic.goto_next({float = false})
+  end,
+  { noremap = true, silent = true, desc = "Go to next warning or error" })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,19 +1,12 @@
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', '<C-a>e', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', 'gdec', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gdef', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gimpl', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<A-r>', vim.lsp.buf.rename, bufopts)
 end
 
 return {
   "neovim/nvim-lspconfig",
   config= function()
-    require('lspconfig').clangd.setup{
+    local lspconfig = require'lspconfig'
+    lspconfig.clangd.setup{
       cmd = {
         'clangd',
         '--all-scopes-completion',
@@ -27,7 +20,18 @@ return {
       on_attach = on_attach,
       root_dir = function(filename)
         local lspc_util = require('lspconfig.util')
-        return lspc_util.root_pattern('.clangd', '.git')(filename)
+        return lspc_util.root_pattern('.git')(filename)
+      end
+    }
+    lspconfig.ols.setup{
+      cmd = {
+        'ols'
+      },
+      filetypes = {'odin'},
+      on_attach = on_attach,
+      root_dir = function(filename)
+        local lspc_util = require('lspconfig.util')
+        return lspc_util.root_pattern('.git')(filename)
       end
     }
   end
