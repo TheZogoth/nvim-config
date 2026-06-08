60f24e6b3dbe73c136907ebdcd80ccce5c4081bf
Johan Ahlqvist <zogoth@gmail.com>
2026-06-08 21:42:40 +0200

    Update Odin LSP configuration

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,19 +13,16 @@ return {
         '-j=6'
       },
       filetypes = {'c', 'cpp', 'cuda'},
-      -- on_attach = function(client, bufnr)
-      --   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-      -- end,
     })
+
     vim.lsp.enable('ols')
     vim.lsp.config('ols', {
       cmd = {
-        'ols'
+        'odinls'
       },
       filetypes = {'odin'},
-      -- on_attach = function(client, bufnr)
-      --   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-      -- end,
     })
+
+    vim.lsp.enable('ts_ls')
   end
 }
